4d3bb7a7784e98645a5530e01e6f3395a93d0576
Lucas, John P <John.P.Lucas@nasa.gov>
2022-10-20 12:16:39 -0400

    [nasa/nos3#92] Relocate mission_setup.sh and update a couple default branches;

--- mission/mission_setup.sh
+++ mission/mission_setup.sh
@@ -4,7 +4,7 @@
 # This script sets up the submodules necessary for a NOS3 new mission setup.
 #
 # It is assumed you have already copied the `mission` directory to a new location
-# and are running this script inside the copy.
+# and are running this script from inside the copy.
 #
 # https://github.com/nasa/nos3
 # 
@@ -15,10 +15,10 @@ git submodule init
 ###
 
 # TODO: Comment out the components not included on the specific mission
-git submodule add -f -b nos3-main https://github.com/nasa-itc/arducam.git ./components/arducam
-git submodule add -f -b nos3-main https://github.com/nasa-itc/generic_reaction_wheel.git ./components/generic_reaction_wheel
-git submodule add -f -b nos3-main https://github.com/nasa-itc/novatel_oem615.git ./components/novatel_oem615
-git submodule add -f -b nos3-main https://github.com/nasa-itc/sample.git ./components/sample
+git submodule add -f -b main https://github.com/nasa-itc/arducam.git ./components/arducam
+git submodule add -f -b main https://github.com/nasa-itc/generic_reaction_wheel.git ./components/generic_reaction_wheel
+git submodule add -f -b main https://github.com/nasa-itc/novatel_oem615.git ./components/novatel_oem615
+git submodule add -f -b main https://github.com/nasa-itc/sample.git ./components/sample
 
 
 ###
@@ -39,7 +39,7 @@ git submodule add -f -b nos3-main https://github.com/nasa-itc/CFS_CI.git ./fsw/a
 #git submodule add -f -b nos3-main https://github.com/nasa-itc/cfs_fm.git ./fsw/apps/fm
 git submodule add -f -b nos3-main https://github.com/nasa-itc/HK.git ./fsw/apps/hk
 git submodule add -f -b nos3-main https://github.com/nasa-itc/HS.git ./fsw/apps/hs
-git submodule add -f -b nos3-main https://github.com/nasa-itc/hwlib.git ./fsw/apps/hwlib
+git submodule add -f -b main https://github.com/nasa-itc/hwlib.git ./fsw/apps/hwlib
 git submodule add -f -b nos3-main https://github.com/nasa-itc/CFS_IO_LIB.git ./fsw/apps/io_lib
 git submodule add -f -b nos3-main https://github.com/nasa-itc/LC.git ./fsw/apps/lc
 #git submodule add -f -b nos3-main https://github.com/nasa-itc/cfs_md.git ./fsw/apps/md
@@ -60,17 +60,17 @@ git submodule add -f -b nos3-main https://github.com/nasa-itc/elf2cfetbl.git ./f
 ### Ground Software (gsw)
 ###
 
-git submodule add -f -b nos3-main https://github.com/nasa-itc/gsw-ait.git ./gsw/ait
-git submodule add -f -b nos3-main https://github.com/nasa-itc/gsw-cosmos.git ./gsw/cosmos
-git submodule add -f -b nos3-main https://github.com/nasa-itc/OrbitInviewPowerPrediction.git ./gsw/OrbitInviewPowerPrediction
+git submodule add -f -b main https://github.com/nasa-itc/gsw-ait.git ./gsw/ait
+git submodule add -f -b main https://github.com/nasa-itc/gsw-cosmos.git ./gsw/cosmos
+git submodule add -f -b main https://github.com/nasa-itc/OrbitInviewPowerPrediction.git ./gsw/OrbitInviewPowerPrediction
 
 
 ###
 ### Simulators (sims)
 ###
 
-git submodule add -f -b nos3-main https://github.com/nasa-itc/nos_time_driver.git ./sims/nos_time_driver
-git submodule add -f -b nos3-main https://github.com/nasa-itc/sim_common.git ./sims/sim_common
-git submodule add -f -b nos3-main https://github.com/nasa-itc/sim_server.git ./sims/sim_server
-git submodule add -f -b nos3-main https://github.com/nasa-itc/sim_terminal.git ./sims/sim_terminal
-git submodule add -f -b nos3-main https://github.com/nasa-itc/truth_42_sim.git ./sims/truth_42_sim
+git submodule add -f -b main https://github.com/nasa-itc/nos_time_driver.git ./sims/nos_time_driver
+git submodule add -f -b main https://github.com/nasa-itc/sim_common.git ./sims/sim_common
+git submodule add -f -b main https://github.com/nasa-itc/sim_server.git ./sims/sim_server
+git submodule add -f -b main https://github.com/nasa-itc/sim_terminal.git ./sims/sim_terminal
+git submodule add -f -b main https://github.com/nasa-itc/truth_42_sim.git ./sims/truth_42_sim
