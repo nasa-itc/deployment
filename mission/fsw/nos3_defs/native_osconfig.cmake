9a421b19dede42a9f2324e83018555755382bf5a
rjbrown6 <Robert.J.Brown@nasa.gov>
2025-03-11 15:29:32 -0400

    [nasa/nos3#519] Remove old Mission Directory, not used.

--- mission/fsw/nos3_defs/native_osconfig.cmake
+++
@@ -1,5 +0,0 @@
-#
-# OSAL configuration addendum when building with SIMULATION=native
-# In this mode, enable the PERMISSIVE option, which allows for easier testing.
-#
-set(OSAL_CONFIG_DEBUG_PERMISSIVE_MODE TRUE)
